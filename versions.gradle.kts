2927a0e41a88a2b6124f457408cfc0c8ed33af10
samgarasx <samgarasx@gmail.com>
2019-12-03 23:07:15 +0100

    Reset Kotlin wrapper packages version

--- versions.gradle.kts
+++ versions.gradle.kts
@@ -10,7 +10,7 @@ project.extra.set("kotlinReactVersion", "16.9.0-pre.83")
 project.extra.set("kotlinReactDomVersion", "16.9.0-pre.83")
 
 // Projects
-project.extra.set("kotlinAntdVersion", "3.20.3-pre.11")
-project.extra.set("kotlinMomentVersion", "2.24.0-pre.6")
-project.extra.set("kotlinReactIntlVersion", "3.1.11-pre.9")
-project.extra.set("kotlinReactResponsiveVersion", "7.0.0-pre.6")
+project.extra.set("kotlinAntdVersion", "3.20.3-pre.0")
+project.extra.set("kotlinMomentVersion", "2.24.0-pre.0")
+project.extra.set("kotlinReactIntlVersion", "3.1.11-pre.0")
+project.extra.set("kotlinReactResponsiveVersion", "7.0.0-pre.0")
