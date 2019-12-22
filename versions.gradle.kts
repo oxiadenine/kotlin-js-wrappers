f384028926c4213799868323038a9e5406f96b5e
samgarasx <samgarasx@gmail.com>
2019-12-22 15:23:16 +0100

    Update kotlin-antd version to 3.20.3-pre.1

--- versions.gradle.kts
+++ versions.gradle.kts
@@ -10,7 +10,7 @@ project.extra.set("kotlinReactVersion", "16.9.0-pre.83")
 project.extra.set("kotlinReactDomVersion", "16.9.0-pre.83")
 
 // Projects
-project.extra.set("kotlinAntdVersion", "3.20.3-pre.0")
+project.extra.set("kotlinAntdVersion", "3.20.3-pre.1")
 project.extra.set("kotlinMomentVersion", "2.24.0-pre.0")
 project.extra.set("kotlinReactIntlVersion", "3.1.11-pre.0")
 project.extra.set("kotlinReactResponsiveVersion", "7.0.0-pre.0")
