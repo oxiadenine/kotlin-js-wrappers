29c3e663d1208fe8f926e29a1eb89625f53664c7
samgarasx <samgarasx@gmail.com>
2020-09-15 17:48:41 +0200

    Add UIKit

---
+++ kotlin-uikit/src/main/kotlin/uikit/UIKit.kt
@@ -0,0 +1,30 @@
+package uikit
+
+@JsModule("uikit")
+@JsNonModule
+external val UIkit: UIKit
+
+external interface UIKit {
+    val util: dynamic
+    val prefix: String
+    val data: String
+    val options: dynamic
+    val version: String
+
+    fun use(plugin: Plugin): UIKit
+    fun mixin(element: dynamic, component: dynamic = definedExternally)
+    fun extend(options: dynamic = definedExternally): dynamic
+    fun update(element: dynamic = definedExternally, e: dynamic = definedExternally)
+
+    fun component(name: String, options: dynamic = definedExternally)
+    fun getComponents(element: dynamic = definedExternally): dynamic
+    fun getComponent(element: dynamic = definedExternally, name: String): dynamic
+    fun connect(node: dynamic = definedExternally)
+    fun disconnect(node: dynamic)
+}
+
+@JsModule("uikit/dist/js/uikit-icons")
+@JsNonModule
+external val Icons: Plugin
+
+typealias Plugin = (uikit: UIKit) -> UIKit
