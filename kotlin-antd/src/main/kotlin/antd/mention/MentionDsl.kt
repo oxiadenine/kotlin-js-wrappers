d17904e6f543bbe2728f8129d3dc5daf7c0fdec0
samgarasx <samgarasx@gmail.com>
2020-12-14 20:43:30 +0100

    Remove deprecated Mention component

--- kotlin-antd/src/main/kotlin/antd/mention/MentionDsl.kt
+++
@@ -1,7 +0,0 @@
-package antd.mention
-
-import kotlinext.js.*
-import react.*
-
-fun RBuilder.mention(handler: RHandler<MentionProps>) = child(MentionComponent::class, handler)
-fun RBuilder.nav(handler: RHandler<NavProps>) = child(MentionComponent.Nav, jsObject {}, handler)
--- kotlin-antd/src/main/kotlin/antd/mention/MentionTypes.kt
+++
@@ -1,5 +0,0 @@
-package antd.mention
-
-typealias MentionPlacement = String /* "top" | "bottom" */
-
-typealias SuggestionItme = Any /* ReactElement | String */
