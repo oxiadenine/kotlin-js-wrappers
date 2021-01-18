90a9cc83efe94e31affa2a5d153c9844784929fb
samgarasx <samgarasx@gmail.com>
2021-01-18 17:13:31 +0100

    Fix ConfigProvider option types

--- kotlin-antd/src/main/kotlin/antd/configprovider/ConfigProviderTypes.kt
+++ kotlin-antd/src/main/kotlin/antd/configprovider/ConfigProviderTypes.kt
@@ -2,8 +2,8 @@ package antd.configprovider
 
 import react.*
 
-typealias SizeType = String? /* "small" | "middle" | "large" */
-typealias DirectionType = String? /* "ltr" | "rtl" */
+typealias SizeType = String /* "small" | "middle" | "large" */
+typealias DirectionType = String /* "ltr" | "rtl" */
 typealias RenderEmptyHandler = (componentName: String?) -> ReactElement
 
 typealias IReactComponent<P> = RClass<P>
