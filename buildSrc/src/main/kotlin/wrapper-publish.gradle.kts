435189b53bd481c4294c84592be8a94e9d7e4329
samgarasx <samgarasx@gmail.com>
2021-03-03 14:58:30 +0100

    Update wrapper publish to include Module artifact and more

--- buildSrc/src/main/kotlin/wrapper-publish.gradle.kts
+++ buildSrc/src/main/kotlin/wrapper-publish.gradle.kts
@@ -1,5 +1,9 @@
+import org.gradle.api.Project
+import org.gradle.api.publish.PublicationArtifact
 import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.publish.maven.internal.artifact.FileBasedMavenArtifact
 import org.gradle.kotlin.dsl.get
+import java.io.File
 
 plugins {
     id("com.jfrog.bintray")
@@ -12,6 +16,7 @@ bintray {
     user = System.getenv("BINTRAY_USER")
     key = System.getenv("BINTRAY_KEY")
     publish = true
+    override = true
 
     pkg = PackageConfig().apply {
         repo = "kotlin-js-wrappers"
@@ -23,15 +28,31 @@ bintray {
         }
     }
 
-    setPublications("kotlin-wrapper")
+    setPublications("kotlinJsWrapper")
 }
 
 publishing {
-    publications.create<MavenPublication>("kotlin-wrapper") {
-        groupId = "com.github.samgarasx"
+    publications.create<MavenPublication>("kotlinJsWrapper") {
+        groupId = project.group.toString()
         artifactId = project.name
         version = pkgVersionName
 
         from(components["kotlin"])
     }
+}
+
+tasks.named("bintrayUpload") {
+    doFirst {
+        project.extensions
+            .getByName<PublishingExtension>("publishing")
+            .publications.withType<MavenPublication>()
+            .all { artifact(project.moduleArtifact(name)) }
+    }
+}
+
+fun Project.moduleArtifact(publicationName: String): PublicationArtifact =
+    ModuleArtifact(buildDir.resolve("publications/$publicationName/module.json"))
+
+class ModuleArtifact(moduleFile: File) : FileBasedMavenArtifact(moduleFile) {
+    override fun getDefaultExtension() = "module"
 }
